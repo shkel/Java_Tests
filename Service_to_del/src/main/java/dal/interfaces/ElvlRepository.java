04c7e4e96cb3a642bba364b43dce093207b125ee
Yulia Shkel <iulia.shkel@gmail.com>
2021-07-28 17:46:47 +0300

    Delete Service_to_del directory

--- Service_to_del/src/main/java/dal/interfaces/ElvlRepository.java
+++
@@ -1,9 +0,0 @@
-package dal.interfaces;
-
-import dal.entities.IsinElvl;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ElvlRepository extends JpaRepository<IsinElvl, Long> {
-
-
-}
--- Service_to_del/src/main/java/dal/interfaces/HistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package dal.interfaces;
-
-import dal.entities.IsinHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoryRepository extends JpaRepository<IsinHistory, Long> {
-
-
-}
